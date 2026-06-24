86b30ec493ca2921bc5bac21a4586a8b43aa9481
Muhammad Maaz Khan <163487911+maazkhan872@users.noreply.github.com>
2026-06-24 16:24:15 +0500

    Add files via upload
    Implemented user logout functionality with session invalidation and secure redirection to the login page.

---
+++ Logout.java
@@ -0,0 +1,27 @@
+package in.sp.main;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/logout")
+public class Logout extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        // Session destroy here
+        HttpSession session = req.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
+        // return login page
+        resp.sendRedirect("index.html");
+    }
+}
